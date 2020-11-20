df27b9a7b42444a23426f5a3bcf723bb0d198501
root <root@LAPTOP-DFQ303PA.localdomain>
2020-11-20 13:28:14 +0700

    Adding multiplication calc

--- node.js
+++ node.js
@@ -3,4 +3,9 @@ var y = 1
 
 var total = x + y
 
-console.log("The totalaaa is " + total)    
+console.log("The total is " + total)    
+//Adding Changes
+
+var x_times_y = x*y;
+
+console.log("X times Y is " + x_times_y);
